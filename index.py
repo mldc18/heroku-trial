654b07b58a3652b6941c0048998998bea4cda677
marc <marc@delacruz.com>
2020-04-05 16:04:04 +0800

    final

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return '<h1>It Works!</h1>'
+    return render_template('home.html')
